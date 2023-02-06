123f309f33109ee213d0b06f422df9bd04a74ee7
Thiago <tmaylon88@gmail.com>
2023-02-06 14:05:41 -0300

    como integrar uma API com Fecth API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://reqres.in/api/users').
+    then(response => response.json()).
+    then(res => setUsers(res.data))
+  }, [])
+  
+  console.log(users)
   return (
     <div className="App">
-      <h1>iniciando com o vite</h1>
+      {users.map((user) => (
+        <div className="card" key={user.id}>
+          <img src={user.avatar} alt={user.first_name} />
+          <p>{user.first_name} {user.last_name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   )
 }
